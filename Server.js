3fbba09e100774d3a1fb252d7dec54ad61e15cba
LoveCTTS <rmeber12@naver.com>
2020-09-10 21:22:16 +0900

    주석 추가(모든 주석이 100% 맞는 설명은 아닐 수도 있으니 참고만 할 것)

--- Server.js
+++ Server.js
@@ -1,29 +1,38 @@
-const express = require('express');
-const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const { v4: uuidV4 } = require('uuid');
+const express = require('express'); //기본적인 서버 구축에 필요한 express 패키지 가져오기
+const app = express(); 
+const server = require('http').Server(app); // http서버를 사용하기위해 http 패키지가져오고, Server메소드에 express관련기능을 사용할 수있는 app변수를 인자로 보냄
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+const io = require('socket.io')(server);// express와 http 기능을 사용하는 server변수를 socket.io 패키지를불러옴과 동시에 인자로 보내서 통신 준비 완료(express기능+http+통신)
+const { v4: uuidV4 } = require('uuid'); // 보안을 위해 uuid 패키지 가져오기(기능상 반드시 필요한 것은 아님)
+
+app.set('view engine', 'ejs'); //view engine을 ejs로 셋팅함으로써, app.get('view engine') 과 같이 명령할 시 ejs가 출력됨.
+app.use(express.static('public')); //public 폴더 밑에 포함된 것(CSS,Image,Javascript code etc..)을 서버에 직접 제공함으로써 사용가능해지게 만듬.
+//ex) 예를들어 public 폴더밑에 현재 script.js 파일이 있으니깐  URL에 127.0.0.1:3000/script.js 이와 같이 사용할 수 있게된다.
+//여러개의 폴더를 포함시키려면 app.use(express.static('test folder')); 과 같이 직접 서버에 제공하고싶은 폴더를 여러번 명령하면 됨.
+
 
 app.get('/', (req, res) => {
   res.redirect(`/${uuidV4()}`);
 });
+// 사용자가 가장 main web page에 접속하면 
+//req(request),res(response) 인자를 받은 콜백함수를 호출하여 express의 response.redirect를 사용해서 '/' 경로를 '/random code'로 redirection되게 설정
 
 app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room });
-});
+}); 
+//room폴더에 있는 모든 파일중에 req.params.room의 값을 roomId에 자장해서 room이라는 ejs파일(view파일)로 값을 전송
+//127.0.0.1:3000/을 치면 자동으로 뒤에 값이 uuid에 의해 랜덤값이 설정된것이 req.params.room에 초기화되어 roomdId에 저장되고, 그것이 room.ejs에 전송되어 room.ejs에서 사용할수있게됨.
 
-io.on('connection', socket => {
+io.on('connection', socket => { 
   socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId);
-    socket.to(roomId).broadcast.emit('user-connected', userId);
+    socket.join(roomId); //랜덤값을 가진 roomId로 room에 join
+    socket.to(roomId).broadcast.emit('user-connected', userId);//나를 제외한 모든 클라이언트들에게 user-connected 됬다고 알림
 
-    socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId);
+    socket.on('disconnect', () => { 
+      socket.to(roomId).broadcast.emit('user-disconnected', userId); //나를 제외한 다른 모든 클라이언트들에게 user-disconnected 됬다고 알림
     });
   });
 });
 
-server.listen(3000);
+
+server.listen(3000); //3000번 포트로 접속가능하도록 서버를 열어둠.
--- public/script.js
+++ public/script.js
@@ -1,56 +1,57 @@
 const socket = io('/');
-const videoGrid = document.getElementById('video-grid');
+const videoGrid = document.getElementById('video-grid');//ejs내의 ID가 video-grid인 것을 찾아서 videoGrid변수에 저장
 const myPeer = new Peer(undefined, {
   host: '/',
   port: '3003'
 });
-const myVideo = document.createElement('video');
-myVideo.muted = true;
-const peers = {};
+//클라이언트 PeerJS에서 3003 포트의 서버에 연결
+const myVideo = document.createElement('video'); //video 요소 생성해서 myVideo에 반환
+myVideo.muted = true; //비디오켜지면 기본적으로 오디오는 음소거로 세팅 (false로 바꾸면 음소거 해제)
+const peers = {}; 
 navigator.mediaDevices.getUserMedia({
   video: true,
   audio: true
 }).then(stream => {
-  addVideoStream(myVideo, stream);
+  addVideoStream(myVideo, stream);//Webcam의 stream을 myVideo에 추가
 
-  myPeer.on('call', call => {
-    call.answer(stream);
-    const video = document.createElement('video');
-    call.on('stream', userVideoStream => {
-      addVideoStream(video, userVideoStream);
+  myPeer.on('call', call => { //peer로 부터 call이 들어오면
+    call.answer(stream); //자신의 stream과함께 답장
+    const video = document.createElement('video'); //video 요소를 만들어서 vido변수에 저장
+    call.on('stream', userVideoStream => { //call이 연결되면
+      addVideoStream(video, userVideoStream); //자신이 아닌 다른 user의 stream을 video에 저장하고 브라우저에 webcam 출력
     });
   });
 
   socket.on('user-connected', userId => {
-    connectToNewUser(userId, stream);
+    connectToNewUser(userId, stream); //유저가 연결되면 userId를 인자로받아서 connectToNewUser함수에 userId와 자신의 stream을 인자로보냄
   });
 });
 
 socket.on('user-disconnected', userId => {
-  if (peers[userId]) peers[userId].close();
+  if (peers[userId]) peers[userId].close(); //유저와의 연결이 끊기면(if(peer[userId])), peerJS의 peers[uuserId].close를 통해 연결 종료
 });
 
 myPeer.on('open', id => {
-  socket.emit('join-room', ROOM_ID, id);
+  socket.emit('join-room', ROOM_ID, id); //PeerJS를 통해 클라이언트가 room에 접속
 });
 
 function connectToNewUser(userId, stream) {
-  const call = myPeer.call(userId, stream);
-  const video = document.createElement('video');
-  call.on('stream', userVideoStream => {
-    addVideoStream(video, userVideoStream);
+  const call = myPeer.call(userId, stream); //PeerJS의 call 메소드에 userId와 자신의 stream(webcam)을 인자로보내서 반환값을 call에 저장
+  const video = document.createElement('video');//video요소 생성
+  call.on('stream', userVideoStream => { //call이 성공하면
+    addVideoStream(video, userVideoStream);//자신의 stream을 비디오에 추가
   });
-  call.on('close', () => {
-    video.remove();
+  call.on('close', () => { //call이 끊어지면
+    video.remove(); //video를 remove
   });
 
   peers[userId] = call;
 }
 
 function addVideoStream(video, stream) {
-  video.srcObject = stream;
-  video.addEventListener('loadedmetadata', () => {
-    video.play();
+  video.srcObject = stream; //자신의 webcam stream을 video.srcObject에 초기화
+  video.addEventListener('loadedmetadata', () => { //동영상과같은 데이터가 event로 발생하면 video.play()함수 실행
+    video.play();// webcam의 stream을 video요소에 넣었기때문에 play해주면 video를 통해 webcam이 동작이됨.
   });
-  videoGrid.append(video);
+  videoGrid.append(video);//ejs에 추가해놓은 style 옵션대로 video를 출력해라.
 }
